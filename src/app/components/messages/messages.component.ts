acf23418f7d83279589d2aee34f7bf0cb7b7724f
gabrielinacioudi <gabsinacio20@gmail.com>
2023-04-03 11:59:00 -0300

    commit inicial

---
+++ src/app/components/messages/messages.component.ts
@@ -0,0 +1,21 @@
+import { MessagesService } from './../../services/messages.service';
+import { Component, OnInit } from '@angular/core';
+
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+  faTimes = faTimes;
+
+
+  constructor(public messagesService: MessagesService) {}
+
+  ngOnInit(): void {}
+}
+
+
